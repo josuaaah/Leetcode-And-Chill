8c0773c05385666b09c9bc8334ba0c7d3a4463b8
Joshua Chew Jian Xiang <59608088+josuaaah@users.noreply.github.com>
2020-12-02 19:25:39 +0800

    Add files via upload

---
+++ AdventOfCode2020/AOC2020_Day2_Q2_PasswordChecker.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+class PasswordChecker {
+
+    static List<String> readInputs() {
+        List<String> inputs = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            inputs.add(scanner.nextLine());
+        }
+        return inputs;
+    }
+
+    static String[] segment(String input) {
+        String delimiters = "-| |: ";
+        return input.split(delimiters);
+    }
+
+    static boolean isValid(String[] segments) {
+        int posOne = Integer.parseInt(segments[0]);
+        int posTwo = Integer.parseInt(segments[1]);
+        char letter = segments[2].charAt(0);
+        String password = segments[3];
+
+        boolean posOneMatch = password.charAt(posOne - 1) == letter;
+        boolean posTwoMatch = password.charAt(posTwo - 1) == letter;
+
+        return (posOneMatch && !posTwoMatch) || (!posOneMatch && posTwoMatch);
+    }
+
+    public static void main(String[] args) {
+        List<String> inputs = readInputs();
+        long numValidPasswords = inputs.stream()
+            .map(input -> segment(input))
+            .filter(segments -> isValid(segments))
+            .count();
+        System.out.println("Number of valid passwords: " + numValidPasswords);
+    }
+}

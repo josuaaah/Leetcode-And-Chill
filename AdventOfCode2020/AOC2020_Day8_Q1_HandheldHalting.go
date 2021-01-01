28fc6149338a21765fbe86a7dc039026a13e9d62
JoshuaChewJianXiang <joshuachewabc@gmail.com>
2021-01-01 14:55:28 +0800

    Implement AOC2020 Day 8 Question 1 (in Go)

---
+++ AdventOfCode2020/AOC2020_Day8_Q1_HandheldHalting.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "log"
+    "os"
+    "strconv"
+)
+
+var accumulator = 0
+var currentInstruction = 0
+
+func main() {
+    instructions := readInstructions()
+    executeFromStart(instructions)
+    fmt.Println(accumulator)
+}
+
+func executeFromStart(instructions map[int] *Instruction) {
+    for instructions[currentInstruction].execution_count < 1 {
+        executeInstruction(instructions[currentInstruction])
+    }
+}
+
+func executeInstruction(instPtr *Instruction) {
+    (*instPtr).execution.execute()
+    (*instPtr).execution_count++
+}
+
+
+func readInstructions() map[int] *Instruction {
+    instructions := make(map[int] *Instruction)
+
+    // Open input data file
+    file, err := os.Open("data.in")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    // Read input line by line
+    scanner := bufio.NewScanner(file)
+    id := 0
+    for scanner.Scan() {
+        instruction := parseInstruction(scanner.Text())
+        instructions[id] = &instruction
+        id++
+    }
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
+
+    return instructions
+}
+
+func parseInstruction(content string) Instruction {
+    instruction_type := content[0:3]
+
+    sign := string(content[4])
+    magnitude, err := strconv.Atoi(content[5:])
+    if err != nil {
+        panic("Invalid argument")
+    }
+
+    var argument int
+    switch sign {
+        case "+":
+            argument = magnitude
+        case "-":
+            argument = -magnitude
+        default:
+            panic("Invalid sign!")
+    }
+
+    switch instruction_type {
+        case "acc":
+            return Instruction{AccExecution{argument}, 0}
+        case "jmp":
+            return Instruction{JmpExecution{argument}, 0}
+        case "nop":
+            return Instruction{NopExecution{argument}, 0}
+        default:
+            panic("Invalid instruction!")
+    }
+}
+
+
+type Execution interface {
+    execute()
+}
+
+type AccExecution struct {
+    argument int
+}
+
+type JmpExecution struct {
+    argument int
+}
+
+type NopExecution struct {
+    argument int
+}
+
+func (ae AccExecution) execute() {
+    accumulator += ae.argument
+    currentInstruction += 1
+}
+
+func (je JmpExecution) execute() {
+    currentInstruction += je.argument
+}
+
+func (ne NopExecution) execute() {
+    currentInstruction += 1
+}
+
+
+type Instruction struct {
+    execution Execution
+    execution_count int
+}
